f765a153a0f70a0dec7fc924728427a91fd75947
ahmadsyahani <111715353+ahmadsyahani@users.noreply.github.com>
2022-09-15 22:35:46 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,46 @@
+<?php 
+
+    $user = [
+        [
+            "username" => "bayu",
+            "password" => "bayu",
+            "img" => "bayu",
+            "alamat" => "Jl. Pacar Cina 23, Surabaya",
+            "tlp" => "0888766678", 
+            "role" => "admin"
+        ],
+        [
+            "username" => "edo",
+            "password" => "edo",
+            "img" => "edo",
+            "alamat" => "Jl. Pacar orang 21A, Surabaya",
+            "tlp" => "0883435221",
+            "role" => "admin"
+        ],
+        [
+            "username" => "rendi",
+            "password" => "rendi",
+            "img" => "rendi",
+            "alamat" => "Jl. Kartanegara 1, Surabaya",
+            "tlp" => "09942352323",
+            "role" => "siswa"
+        ],
+        [
+            "username" => "samuel",
+            "password" => "samuel",
+            "img" => "samuel",
+            "alamat" => "Jl. Majapahit No 32, Surabaya",
+            "tlp" => "093532212424",
+            "role" => "siswa"
+        ],
+        [
+            "username" => "yanto",
+            "password" => "yanto",
+            "img" => "yanto",
+            "alamat" => "Jl. Asem Jawa Blok F, Surabaya",
+            "tlp" => "093242323111",
+            "role" => "siswa"
+        ],
+    ];
+
+?>
